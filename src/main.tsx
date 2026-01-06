b464c6625e80640af30c5e75471f95e00b4235f1
smelik81 <artem.smelyanskiy81@gmail.com>
2026-01-06 13:35:26 +0200

    added all leibrary

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
+//import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-createRoot(document.getElementById("root")!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
